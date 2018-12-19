31d78b2f8b0478260122c80b4d424eb385702acc
Selendireyko <vkv07@mail.ru>
2018-12-19 19:49:48 +0300

    Lab6

---
+++ Lab6/Lab6.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <sys/shm.h>
+
+#define FILE "FILE_MEMMORY"
+
+pthread_mutex_t mut;
+pthread_cond_t per;
+
+void* thread_func ()
+{
+    key_t shmkey = ftok("/tmp", 'z');
+    int smid = shmget (shmkey, 50, 0666);
+    if (smid == -1)
+    {
+        printf ("MEMORY ERROR\n");
+        exit (0);
+    }
+    char* ad = shmat (smid, NULL, 0);
+    if (ad == (char*)-1)
+    {
+        printf ("Connecntion ERROR\n");
+        exit (0);
+    }
+    time_t timer;
+    while (1)
+    {
+        timer = time(0);
+        pthread_mutex_lock(&mut);
+        sprintf(ad, "%s", ctime(&timer));
+        pthread_cond_broadcast (&per);
+        pthread_mutex_unlock(&mut);
+        sleep (1);
+    }
+    exit (0);
+}
+
+int main()
+{
+    pthread_t pth;
+    pthread_mutex_init (&mut, NULL);
+    int st = pthread_create (&pth, NULL, thread_func, NULL);
+    if (st != 0)
+    {
+        printf ("%d Creat potok ERROR\n", st);
+        exit (0);
+    }
+    key_t shmkey = ftok("/tmp", 'z');
+    int smid = shmget (shmkey, 50, IPC_CREAT|0666);
+    if (smid == -1)
+    {
+        printf ("MEMORY ERROR\n");
+        exit (0);
+    }
+    char* ad = shmat (smid, NULL, 0);
+    if (ad == (char*)-1)
+    {
+        printf ("Connecntion ERROR\n");
+        exit (0);
+    }
+    while (1)
+    {
+        sleep(1);
+        pthread_mutex_lock (&mut);
+        pthread_cond_wait(&per, &mut);
+        printf ("%s\n", ad);
+        pthread_mutex_unlock(&mut);
+    }
+    pthread_mutex_destroy(&mut);
+    exit (0);
+}
